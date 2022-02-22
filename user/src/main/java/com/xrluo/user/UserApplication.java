fe2222107b49e4f4d0d37a9a8ff4a636b64939ad
yxz4ever <yxz4ever@sina.com>
2022-02-22 10:04:53 +0800

    使用数据库

--- user/src/main/java/com/xrluo/user/UserApplication.java
+++ user/src/main/java/com/xrluo/user/UserApplication.java
@@ -1,9 +1,11 @@
 package com.xrluo.user;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.xrluo.user.dao"})
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class,args);
--- user/src/main/java/com/xrluo/user/dao/UserDao.java
+++ user/src/main/java/com/xrluo/user/dao/UserDao.java
@@ -1,7 +1,9 @@
 package com.xrluo.user.dao;
 
 import com.xrluo.user.entity.SysUser;
+import org.springframework.stereotype.Repository;
 
+@Repository()
 public interface UserDao {
     SysUser getById(Long id);
 }
